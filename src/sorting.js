6a1330a938cacdc0db005ce211073484ac8d4ec1
OlenaKurchenko <31536428+OlenaKurchenko@users.noreply.github.com>
2017-09-13 00:01:24 +0300

    - Removed unused code

--- src/sorting.js
+++ src/sorting.js
@@ -14,25 +14,8 @@ class Sorting{
 	}
 }
 
-class Sorting1{
-	bubble1(unsortedArray) {
-		var sortingArray = unsortedArray.slice();
-		for (var i = (sortingArray.length - 1); i >= 0; i--) {
-        	for (var j = (sortingArray.length - i); j > 0; j--) {
-            	if (sortingArray[j] < sortingArray[j - 1]) {
-                	var tmp = sortingArray[j];
-                	sortingArray[j] = sortingArray[j - 1];
-                	sortingArray[j - 1] = tmp;
-				}
-			}
-		}
-		return sortingArray;
-	}
-}
-
 module.exports = {
-	Sorting : Sorting,
-	Sorting1 : Sorting1
+	Sorting : Sorting
 }
 
 
